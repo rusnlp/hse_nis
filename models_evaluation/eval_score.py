8f5556b2ecb4c3266161d6d20571357fc8988e2a
PetrFil <50084918+PetrFil@users.noreply.github.com>
2020-02-22 02:41:40 +0300

    eval_score

---
+++ models_evaluation/eval_score.py
@@ -0,0 +1,65 @@
+"""""
+На вход принимается путь к таблице tsv. В ней обязательно есть 2 столбца: 
+Target article - статья запроса 
+Results - полученные ближайшие статьи
+Также есть столбцы с оценками моделей. Их число может варьироваться и они должны иметь следующий вид:
+"Muse 21", где "Muse" - название модели, "21" - id или порядковый номер аннотатора
+На выходе получаем Krippendorff's alpha и среднюю оценку по каждой модели
+
+python eval_score.py --path='C:/data/RUSNLP_MAP_EVAL.TSV'
+"""""
+
+import krippendorff
+import pandas as pd
+from pandas import DataFrame
+import re
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Средняя оценка аннотаторов и коэффициент их согласия для каждой модели')
+    parser.add_argument('--path', type=str, required=True,
+                        help='Язык, для которго разбираем, нужен для определения словаря в маппинге (ru/en)')
+    return parser.parse_args()
+
+
+def open_excel(path):
+    df = pd.read_csv(path, sep='\t', header=0)
+    return df
+
+
+# находим названия моделей с помощью регулярных выражений
+def find_models(path):
+    dataframe = open_excel(path)
+    columns = dataframe.columns
+    model_names, column_names = set(), []
+    for column in columns:
+        if 'Unnamed' in column:  # чтобы не считывался какой-нибудь пустой столбец
+            break
+        name = re.findall('(.+) [1-9]+$', column)
+        model_names.update(name)
+        if name:
+            column_names += [column]
+    return model_names, column_names
+
+
+def mean_and_krip(path):
+    models, columns = find_models(path)
+    df = open_excel(path)
+    for model in models:
+        model_columns = []
+        for column in columns:
+            if model in column:
+                model_columns += [column]
+        model_df = df[model_columns]
+        models_mean = model_df.mean()
+        krip = krippendorff.alpha(model_df)
+        print('Krippendorff\'s alpha coefficient for {}:'.format(model), krip)
+        total_mean = models_mean.sum()/len(models_mean)
+        print('Mean value for {}:'.format(model), total_mean)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    mean_and_krip(args.path)
