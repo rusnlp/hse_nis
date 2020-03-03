babd07eb9b6f77404b97d6d7d903d29d27e57df7
PetrFil <50084918+PetrFil@users.noreply.github.com>
2020-03-03 16:42:45 +0300

    eval_score + converter

---
+++ models_evaluation/converter.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import argparse
+# python converter.py --path=eval/Muse_eval.csv --save_as=converted_eval/Muse.csv --bin=True
+# python converter.py --path=eval/Baseline_eval.csv --save_as=converted_eval/Baseline.csv --bin=True
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='converter')
+    parser.add_argument('--path', type=str,
+                        required=True, help='Путь к исходному файлу с оценками')
+    parser.add_argument('--save_as', type=str,
+                        required=True, help='Куда сохранить конвертированный файл')
+    parser.add_argument('--sep', type=str,
+                        help='Тип сепаратора в csv-файле')
+    parser.add_argument('--bin', type=str,
+                        help='Перевод 5-балльной системы оценки в бинарную')
+    return parser.parse_args()
+
+
+def binary_values(df):
+    df = df.apply({lambda x: 0 if (int(x) < 3) else 1})
+    return df
+
+
+def converter(input_path, output_path, sep, binar):
+    if not sep:
+        sep = ','
+    df = pd.read_csv(input_path, sep=sep, header=None)
+    df = df.drop([0]).drop([0, 1], axis=1)
+    if binar:
+        df = binary_values(df)
+    df.to_csv(output_path, index=False, sep=',', header=None)
+
+
+def main():
+    args = parse_args()
+    converter(args.path, args.save_as, args.sep, args.bin)
+
+
+if __name__ == '__main__':
+    main()
--- models_evaluation/eval_score.py
+++ models_evaluation/eval_score.py
@@ -1,64 +1,33 @@
-"""""
-На вход принимается путь к таблице tsv. В ней обязательно есть 2 столбца: 
-Target article - статья запроса 
-Results - полученные ближайшие статьи
-Также есть столбцы с оценками моделей. Их число может варьироваться и они должны иметь 
-следующий вид:
-"Muse 21", где "Muse" - название модели, "21" - id или порядковый номер аннотатора
-На выходе получаем Krippendorff's alpha и среднюю оценку по каждой модели
+"""
+На вход подается csv-файл c бинарными оценками семантической близости найденных статей к статье запроса
+На выходе получаем альфу Криппендорффа и среднюю оценку
+"""
+# python eval_score.py --path=converted_eval/Baseline.csv
+# python eval_score.py --path=converted_eval/Muse.csv
 
-python eval_score.py --path=C:/data/RUSNLP_MAP_EVAL.TSV
-"""""
 
 import argparse
-import re
 import krippendorff
 import pandas as pd
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description='Средняя оценка аннотаторов и коэффициент их согласия для каждой модели')
-    parser.add_argument('--path', type=str, required=True, help='Путь к файлу с оценками')
+        description='Средняя оценка аннотаторов и коэффициент их согласия')
+    parser.add_argument('--path', type=str, required=True, help='Путь к csv-файлу')
     return parser.parse_args()
 
 
-def open_excel(path):
-    df = pd.read_csv(path, sep='\t', header=0)
+def score(path):
+    df = pd.read_csv(path, sep=',', header=None)
+    mean = df.mean()
+    total_mean = sum([i for i in mean])/len(mean)
+    krip = krippendorff.alpha(df.T)
+    print('Krippendorff\'s alpha coefficient:', krip)
+    print('Mean value:', total_mean)
     return df
-
-
-# находим названия моделей с помощью регулярных выражений
-def find_models(path):
-    dataframe = open_excel(path)
-    columns = dataframe.columns
-    model_names, column_names = set(), []
-    for column in columns:
-        if 'Unnamed' in column:  # чтобы не считывался какой-нибудь пустой столбец
-            break
-        name = re.findall('(.+) [1-9]+$', column)
-        model_names.update(name)
-        if name:
-            column_names += [column]
-    return model_names, column_names
-
-
-def mean_and_krip(path):
-    models, columns = find_models(path)
-    df = open_excel(path)
-    for model in models:
-        model_columns = []
-        for column in columns:
-            if model in column:
-                model_columns += [column]
-        model_df = df[model_columns]
-        models_mean = model_df.mean()
-        krip = krippendorff.alpha(model_df.T)
-        print('Krippendorff\'s alpha coefficient for {}:'.format(model), krip)
-        total_mean = models_mean.sum() / len(models_mean)
-        print('Mean value for {}:'.format(model), total_mean)
 
 
 if __name__ == '__main__':
     args = parse_args()
-    mean_and_krip(args.path)
+    score(args.path)
