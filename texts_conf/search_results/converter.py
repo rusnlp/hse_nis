aed13ab7a15065c8f9abe39b4f40a0f338429af9
PetrFil <50084918+PetrFil@users.noreply.github.com>
2020-03-30 20:52:38 +0300

    url and id

--- texts_conf/search_results/converter.py
+++ texts_conf/search_results/converter.py
@@ -6,5 +6,5 @@ for path in paths:
     with open(path, 'r', encoding='utf-8') as file:
         df = pd.read_csv(file, sep='\t', header=None)
         df = df.drop(df.columns[[2, 3]], axis=1)
-        df[0] = df[0].apply(lambda x: int(x))
+        df[0] = df[0].apply(lambda x: str(x)[:2])
         df.to_csv('{}_id_and_urls.tsv'.format(path[:-4]), sep='\t', index=False, header=False)
