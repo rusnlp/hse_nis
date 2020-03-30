7fee4e90c213d63c6597579641c5f464a5cf0e3a
PetrFil <50084918+PetrFil@users.noreply.github.com>
2020-03-30 20:44:22 +0300

    only id and url

---
+++ texts_conf/search_results/converter.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+paths = ['results_muse.txt', 'results_proj.txt', 'results_trans.txt', 'results_vecmap.txt']
+for path in paths:
+    with open(path, 'r', encoding='utf-8') as file:
+        df = pd.read_csv(file, sep='\t', header=None)
+        df = df.drop(df.columns[[2, 3]], axis=1)
+        df[0] = df[0].apply(lambda x: int(x))
+        df.to_csv('{}_id_and_urls.tsv'.format(path[:-4]), sep='\t', index=False, header=False)
